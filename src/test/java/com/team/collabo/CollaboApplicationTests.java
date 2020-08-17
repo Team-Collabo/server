5e31b53f6e73671dbf3553fec78e13be5fb1bd3e
Seungmin <hsm012362@gmail.com>
2020-08-18 02:07:43 +0900

    project init

---
+++ src/test/java/com/team/collabo/CollaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.collabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
