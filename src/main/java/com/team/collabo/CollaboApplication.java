e1dddc598ada60e1ebee94bd58f80cfc56615a7a
Seungmin <hsm012362@gmail.com>
2020-08-18 14:46:03 +0900

    add swagger

--- src/main/java/com/team/collabo/CollaboApplication.java
+++ src/main/java/com/team/collabo/CollaboApplication.java
@@ -7,22 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @EnableJpaAuditing
 @SpringBootApplication
 public class CollaboApplication {
 
-	public static void main(String... args) {
-		SpringApplication.run(CollaboApplication.class, args);
-	}
+    public static void main(String... args) {
+        SpringApplication.run(CollaboApplication.class, args);
+    }
 
-	@GetMapping("api/hello")
-	public String hello() {
-		return "hello! Team Collabo!";
-	}
+    @GetMapping("api/hello")
+    public String hello() {
+        return "hello! Team Collabo!";
+    }
 
-	@GetMapping("api/hello/{name}")
-	public String greeting(@PathVariable("name") final String name) {
-		return String.format("hello! %s", name);
-	}
+    @GetMapping("api/hello/{name}")
+    public String greeting(@PathVariable("name") final String name) {
+        return String.format("hello! %s", name);
+    }
 }
