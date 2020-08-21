6ce60ae2ff3fece4f8db7842795d0b30f6f1359d
Seungmin <hsm012362@gmail.com>
2020-08-22 04:57:48 +0900

    fix 🔥 extends jparepository to some repo & fix error

--- src/main/java/com/team/collabo/team/repository/DashBoardRepository.java
+++ src/main/java/com/team/collabo/team/repository/DashBoardRepository.java
@@ -1,4 +1,7 @@
 package com.team.collabo.team.repository;
 
-public interface DashBoardRepository {
+import com.team.collabo.team.model.DashBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DashBoardRepository extends JpaRepository<DashBoard, Long> {
 }
